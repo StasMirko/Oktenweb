75bce12e0233bc602b6508387b330cf2a33ba7c4
Стас <stasmq@gmail.com>
2021-01-21 20:42:49 +0200

    Cooomit

---
+++ Git/Git.js
@@ -0,0 +1,27 @@
+console.log('Hello worldd!');
+
+// 1. VCS => Import into version control => Share
+
+// Quick setup — if you’ve done this kind of thing before
+// or
+// https://github.com/StasMirko/Git_lesson.git
+//     Get started by creating a new file or uploading an existing file. We recommend every repository
+//     include a README, LICENSE, and .gitignore.
+//
+// …or create a new repository on the command line
+// echo "# Git_lesson" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/StasMirko/Git_lesson.git
+//     git push -u origin main
+//
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/StasMirko/Git_lesson.git
+//     git branch -M main
+// git push -u origin main
+// …or import code from another repository
+// You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
+
+ctrl + sh + k
